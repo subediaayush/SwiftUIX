3bfd5f823b453ac0f2b6fd6ff9d4cd5aad255a42
Aayush Subedi <subediaayush@users.noreply.github.com>
2021-02-01 14:30:17 +0545

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIX",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v10),
         .macOS(.v10_15),
         .tvOS(.v13),
         .watchOS(.v6)
